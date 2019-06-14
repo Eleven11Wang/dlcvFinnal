6b9cda1a3a69b5285da779190217f0391a6aad93
Eleven11Wang <janewang4332@gmail.com>
2019-06-14 16:22:55 +0800

    sad

--- p3_test2.py
+++ p3_test2.py
@@ -30,36 +30,8 @@ def tester(feature_extractor, class_classifier, domain_classifier, source_datalo
     domain_correct = 0.0
     tgt_correct = 0.0
     src_correct = 0.0
-    num1=0
-
-    for batch_idx, sdata in enumerate(source_dataloader):
-        if(batch_idx> 100):
-            break
-
-        # setup hyperparameters
-        p = float(batch_idx) / len(source_dataloader)
-        constant = 2. / (1. + np.exp(-10 * p)) - 1.
-
-        input1, label1, filenames = sdata
-        
-        input1, label1 = Variable(input1.cuda()), Variable(label1.cuda())
-        src_labels = Variable(torch.zeros((input1.size()[0])).type(torch.LongTensor).cuda())
-        
-        feature=feature_extractor(input1)
-        feature=feature.view(-1,2048)
-        output1 = class_classifier(feature)
-
-        pred1 = output1.data.max(1, keepdim = True)[1]
-        
-        source_correct += pred1.eq(label1.data.view_as(pred1)).cpu().sum()
-        num1+=64
-        
-        src_preds = domain_classifier(feature, constant)
-        src_preds = src_preds.data.max(1, keepdim= True)[1]
-        src_correct += src_preds.eq(src_labels.data.view_as(src_preds)).cpu().sum()
-    acc1=100. * float(source_correct) / num1
-    #len(source_dataloader.dataset)
-    print('\nSource Accuracy: {}/{} ({:.4f}%)\n'.format(source_correct, num1,acc1 ))          
+    
+       
           
     num2=0  
     for batch_idx, tdata in enumerate(target_dataloader):
@@ -82,23 +54,23 @@ def tester(feature_extractor, class_classifier, domain_classifier, source_datalo
         
         target_correct += pred2.eq(label2.data.view_as(pred2)).cpu().sum()
         num2+=64
-        tgt_preds = domain_classifier(feature, constant)
-        tgt_preds = tgt_preds.data.max(1, keepdim=True)[1]
-        tgt_correct += tgt_preds.eq(tgt_labels.data.view_as(tgt_preds)).cpu().sum()
+        #tgt_preds = domain_classifier(feature, constant)
+        #tgt_preds = tgt_preds.data.max(1, keepdim=True)[1]
+        #tgt_correct += tgt_preds.eq(tgt_labels.data.view_as(tgt_preds)).cpu().sum()
     
     acc2=100. * float(target_correct) / num2
     #len(target_dataloader.dataset)
     print("\nTarget Accuracy: {}/{} ({:.4f}%)\n".format(target_correct, num2, acc2))
     
               
-    domain_correct = tgt_correct + src_correct
+    #domain_correct = tgt_correct + src_correct
     
 
 
-    print('Domain Accuracy: {}/{} ({:.4f}%)\n'.format(domain_correct, num1+num2,
-        100. * float(domain_correct) / (num1+num2)))
+    #print('Domain Accuracy: {}/{} ({:.4f}%)\n'.format(domain_correct, num1+num2,
+        #100. * float(domain_correct) / (num1+num2)))
 
-    return (acc1,acc2)
+    return acc2
 
 
 
